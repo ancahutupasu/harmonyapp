d48a129a04a215585287b159422395b73ee76149
Ravneet <87025282+2945245@users.noreply.github.com>
2022-05-03 10:14:15 +0200

    Add files via upload

--- Harmony/app/src/main/java/com/example/harmony/MainActivity.java
+++ Harmony/app/src/main/java/com/example/harmony/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.harmony;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_cart);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v->{
+            Intent intent = new Intent(this, SignUp.class);
+            startActivity(intent);
+        });
     }
 }
