ced40cb7eb2ada1e903438d5d3a42e5c69e2b457
Ravneet <87025282+2945245@users.noreply.github.com>
2022-05-14 20:01:24 +0200

    Add files via upload

---
+++ Harmony/app/src/main/java/com/example/harmony/PersonInformation.java
@@ -0,0 +1,22 @@
+package com.example.harmony;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class PersonInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_information);
+
+        Button button = findViewById(R.id.Done);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MyAccount.class);
+            startActivity(intent);
+        });
+    }
+}
