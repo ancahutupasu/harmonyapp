c05c268f5fc5f9a1f8b43c9091962eb0c8d3273d
Elena <agape.ancaelena@yahoo.com>
2022-05-15 20:36:17 +0200

    folder structure

---
+++ Harmony/app/src/main/java/com/example/harmony/ui/my_cart/MyCart.java
@@ -0,0 +1,40 @@
+package com.example.harmony.ui.my_cart;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.harmony.R;
+
+public class MyCart extends Fragment {
+
+    private MyCartViewModel mViewModel;
+
+    public static MyCart newInstance() {
+        return new MyCart();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_cart, container, false);
+    }
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MyCartViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Harmony/app/src/main/java/com/example/harmony/ui/my_cart/MyCartViewModel.java
@@ -0,0 +1,12 @@
+package com.example.harmony.ui.my_cart;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyCartViewModel extends ViewModel {
+
+    public static MyCart newInstance() {
+        return new MyCart();
+    }
+
+
+}
