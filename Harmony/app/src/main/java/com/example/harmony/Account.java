e43b46e44f3ab3a6ca680a8e07794a58418b8336
2945245 <87025282+2945245@users.noreply.github.com>
2022-05-15 18:40:40 +0200

    making fragments

--- Harmony/app/src/main/java/com/example/harmony/Account.java
+++ Harmony/app/src/main/java/com/example/harmony/Account.java
@@ -2,6 +2,7 @@ package com.example.harmony;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +12,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Account extends Fragment {
 
     private AccountViewModel mViewModel;
+    private Button button;
 
     public static Account newInstance() {
         return new Account();
@@ -23,14 +27,26 @@ public class Account extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View rootView=inflater.inflate(R.layout.fragment_fragment_account, container, false);
+
+       // button= rootView.findViewById(R.id.PersonInformation);
+       // button.setOnClickListener(new View.OnClickListener() {
+           // @Override
+            //public void onClick(View view) {
+              //  Toast.makeText(getActivity(),"Hey",Toast.LENGTH_SHORT);
+           // }
+        //});
+        return rootView;
+
     }
 
-    @Override
+
+    /*@Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
         // TODO: Use the ViewModel
-    }
+    }*/
 
 }
+
