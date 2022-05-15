8a9f4b0b815760567619fdb851337a4dfaa0ed6a
2945245 <87025282+2945245@users.noreply.github.com>
2022-05-15 19:26:58 +0200

    making fragments

---
+++ Harmony/app/src/main/java/com/example/harmony/ui/PersoninformationfragmentViewModel.java
@@ -0,0 +1,7 @@
+package com.example.harmony.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class PersoninformationfragmentViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
