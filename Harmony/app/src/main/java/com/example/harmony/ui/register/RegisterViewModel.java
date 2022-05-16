1e2d0ca29da8d7d87a18bff7166023c504d93c98
Timothy Engkar <56295945+AsuraEyes@users.noreply.github.com>
2022-05-16 17:28:35 +0200

    Add files via upload

---
+++ Harmony/app/src/main/java/com/example/harmony/ui/register/RegisterViewModel.java
@@ -0,0 +1,34 @@
+package com.example.harmony.ui.register;
+
+import android.app.Activity;
+import android.app.Application;
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.harmony.data.user.UserRepository;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+public class RegisterViewModel extends AndroidViewModel {
+
+    private final UserRepository userRepository;
+
+    public RegisterViewModel(Application app) {
+        super(app);
+        userRepository = UserRepository.getInstance(app);
+    }
+
+    public void registerAccount(Activity activity, String email, String password)
+    {
+        userRepository.registerAccount(activity,email,password);
+    }
+
+    public void setSignInPressed(Boolean isSignInPressed) {
+        userRepository.setSignInPressed(isSignInPressed);
+    }
+
+    public void firebaseAuthWithGoogle(boolean isRegister, GoogleSignInAccount account, FragmentActivity activity) {
+        userRepository.firebaseAuthWithGoogle(isRegister,account,activity);
+    }
+}
