8f646755b8ec41704b6b40201ecb054e4181da43
Elena <agape.ancaelena@yahoo.com>
2022-05-17 12:15:46 +0200

    missing pages

---
+++ Harmony/app/src/main/java/com/example/harmony/ui/contact_us/ContactUsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.harmony.ui.contact_us;
+
+import androidx.lifecycle.ViewModel;
+
+public class ContactUsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ Harmony/app/src/main/java/com/example/harmony/ui/faq/FAQFragment.java
@@ -0,0 +1,38 @@
+package com.example.harmony.ui.faq;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.harmony.R;
+
+public class FAQFragment extends Fragment {
+
+    private FAQViewModel mViewModel;
+
+    public static FAQFragment newInstance() {
+        return new FAQFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_f_a_q, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FAQViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
